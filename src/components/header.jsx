c4c6267495b0e7c7bf688035473e14461ec34a30
gapavoyan <gap_avoyan@mail.ru>
2023-04-24 17:34:25 +0400

    create newsAggregator

---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <header class="bg-gray-900  text-white">
+                <div class="container mx-auto py-4 px-8 flex items-center justify-between">
+                    <a href="#" class="text-3xl font-semibold">News App</a>
+
+                    <div class="relative mx-auto text-gray-600">
+                        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                            type="search" name="search" placeholder="Search" />
+                        <button type="submit"
+                            class="absolute right-0 top-0 mt-2 mr-4 text-purple-500 hover:text-purple-700">
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
